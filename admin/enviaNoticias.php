34073ba5066273051d1952b864472e39d7a0e1e0
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-06-11 10:49:19 -0300

    Modificação do INSERT para UPDATE do alterar foto

--- admin/enviaNoticias.php
+++ admin/enviaNoticias.php
@@ -21,42 +21,19 @@ $nome_arquivo = $arquivo['name'];
 $destino = "imgNoticias/" . $arquivo['name'];
 if (move_uploaded_file($arquivo['tmp_name'], $destino)) {
     header('Location: noticiaisAdmin.php');
-    $sql = $pdo->prepare("INSERT INTO tblfotos_noticias VALUES (null, :nome_arquivo)");
-    $sql->bindParam(':nome_arquivo', $nome_arquivo);
-    $sql->execute();
+    
+    // Obtém o ID do último registro
+    $sql_last_id = $pdo->prepare("SELECT MAX(id) as last_id FROM tblfotos_noticias");
+    $sql_last_id->execute();
+    $last_id_result = $sql_last_id->fetch(PDO::FETCH_ASSOC);
+    $last_id = $last_id_result['last_id'];
+
+    // Atualiza o arquivo do último registro
+    $sql_update = $pdo->prepare("UPDATE tblfotos_noticias SET arquivo = :nome_arquivo WHERE id = :last_id");
+    $sql_update->bindParam(':nome_arquivo', $nome_arquivo);
+    $sql_update->bindParam(':last_id', $last_id);
+    $sql_update->execute();
 
 } else {
     header('Location: noticiaisAdmin.php');
 }
-/*
-require('../db/conexao.php');
-
-$pasta = "imgArquivos";
-
-if(!is_dir($pasta)){
-    mkdir($pasta);
-}
-
-$dado = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-if (empty($_FILES['arquivo'])) {
-    // O arquivo não foi enviado, trate o erro adequadamente
-    echo "Nenhum arquivo selecionado.";
-    exit;
-}
-
-$arquivo = $_FILES['arquivo']; // Adicione esta linha
-
-$nome_arquivo = $arquivo['name'];
-$destino = "imgArquivos/" . $arquivo['name'];
-if (move_uploaded_file($arquivo['tmp_name'], $destino)) {
-    header('Location: noticiaisAdmin.php');
-    $sql = $pdo->prepare("INSERT INTO tblfotos_noticias VALUES (null, :nome_arquivo)");
-    $sql->bindParam(':nome_arquivo', $nome_arquivo);
-    $sql->execute();
-
-} else {
-    header('Location: noticiaisAdmin.php');
-}
-
-*/
-?>
